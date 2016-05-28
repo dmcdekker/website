272f5dc219fd374a4e28e961a7573eaa774e93b4
Denise <ddekker@mills.edu>
2016-05-28 23:10:23 +0000

    header footer and home styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   
   
-  root 'static_pages#home'
-
-  get 'static_pages/about'
-
-  get 'static_pages/work'
+  root             'static_pages#home'
+  get 'about'   => 'static_pages#about'
+  get 'work' => 'static_pages#work'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
