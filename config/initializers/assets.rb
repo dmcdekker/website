9092cdf47c69ae6043f6873b22275ddc50eb9447
Denise <ddekker@mills.edu>
2016-05-31 23:55:25 +0000

    about page image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( custom.css )
 Rails.application.config.assets.precompile += %w( static_page.css )
+Rails.application.config.assets.precompile += %w( application.css )
