5e4c60bbbe28854f606fd2a7cfd53508604a9b02
Denise <ddekker@mills.edu>
2016-08-20 18:26:41 +0000

    add javascript and modal back in for ux

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,18 @@
 /* copy loaded thumbnails into carousel */
-$('.row .thumbnail').on('load', function() {
+$('.thumbnail').on('load', function() {
   
 }).each(function(i) {
   if(this.complete) {
-  	var item = $('<div class="item"></div>');
+    var item = $('<div class="item"></div>');
     var itemDiv = $(this).parents('div');
     var title = $(this).parent('a').attr("title");
     
     item.attr("title",title);
-  	$(itemDiv.html()).appendTo(item);
-  	item.appendTo('.carousel-inner'); 
+    $(itemDiv.html()).appendTo(item);
+    item.appendTo('.carousel-inner'); 
     if (i==0){ // set first item active
      item.addClass('active');
+    
     }
   }
 });
@@ -19,18 +20,18 @@ $('.row .thumbnail').on('load', function() {
 /* activate the carousel */
 $('#modalCarousel').carousel({interval:false});
 
-/* change modal title when slide changes */
-$('#modalCarousel').on('slid.bs.carousel', function () {
-  $('.modal-title').html($(this).find('.active').attr("title"));
-})
-
 /* when clicking a thumbnail */
-$('.row .thumbnail').click(function(e){
+$('.thumbnail').click(function(e){
     e.preventDefault();
     var idx = $(this).parents('div').index();
-  	var id = parseInt(idx);
-  	$('#myModal').modal('show'); // show the modal
+    var id = parseInt(idx);
+    $('#myModal').modal('show'); // show the modal
     $('#modalCarousel').carousel(id); // slide carousel to selected
-  	
-});
+});    
+
+
+
+
+
+
 
