a26de57539fb6d33aeff9e40ba200e8135d73fbc
Denise <ddekker@mills.edu>
2017-05-08 07:36:26 +0000

    jquery addition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,9 +10,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require bootstrap
-//= require leaflet
-//= require leaflet.markercluster
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require_tree .
+//= require bootstrap
